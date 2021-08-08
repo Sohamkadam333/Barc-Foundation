fcc160d3d2e149c17a5813386b7f728bc1ac36c1
Soham Kadam <60404913+Sohamkadam333@users.noreply.github.com>
2021-08-09 00:57:57 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+// routes
+const routes = require('./routes/routes.js');
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connecting to database
+mongoose.connect(
+  'mongodb+srv://eric:test@123@cluster0.vwtix.mongodb.net/barc-foundation?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(routes, (err) => {
+  console.log(err);
+});
+
+app.listen(5000, (err) => {
+  console.log('serving on port 5000');
+});
