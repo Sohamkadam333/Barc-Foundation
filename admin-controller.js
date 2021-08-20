250877614a0e8b690b68fbc63be48824b4de3ce9
Sohamkadam333 <sohamkadam58@gmail.com>
2021-08-21 00:54:08 +0530

    Images added

---
+++ admin-controller.js
@@ -0,0 +1,39 @@
+const UserModel = require('../models/DbSchema-user');
+const NewsletterModel = require('../models/DbSchema-newsletter');
+
+const getHomePage = (req, res) => {
+  res.send('Hello express');
+};
+
+const addUserToDatabase = (req, res) => {
+  const { user_name, user_email, user_message } =
+    req.body;
+  const user = new UserModel({
+    user_name: user_name,
+    user_email: user_email,
+    user_message: user_message,
+  });
+
+  user
+    .save('Data Inserted Successfully')
+    .then((response) => console.log('success'))
+    .catch((err) => console.log(err));
+};
+
+const addUserToNewsLetter = (req, res) => {
+  const { user_email } = req.body;
+  const newsLetter = new NewsletterModel({
+    user_email: user_email,
+  });
+
+  newsLetter
+    .save('Data Inserted Successfully')
+    .then((response) => console.log('success'))
+    .catch((err) => console.log(err));
+};
+
+module.exports = {
+  getHomePage,
+  addUserToDatabase,
+  addUserToNewsLetter,
+};
